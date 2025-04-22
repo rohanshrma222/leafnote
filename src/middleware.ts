801af605e956093b41a816e25425d27fe67778b3
unknown <hackerviruse04@gmail.com>
2025-04-22 15:16:06 +0530

    integrated supabase

---
+++ src/middleware.ts
@@ -0,0 +1,45 @@
+import { NextResponse, type NextRequest } from 'next/server'
+import { createServerClient } from '@supabase/ssr'
+
+export async function middleware(request: NextRequest) {
+  return await updateSession(request)
+}
+
+export const config = {
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
+}
+
+export async function updateSession(request: NextRequest) {
+  let supabaseResponse = NextResponse.next({
+    request,
+  })
+
+//   const supabase = createServerClient(
+//     process.env.SUPABASE_URL!,
+//     process.env.SUPABASE_ANON_KEY!,
+//     {
+//       cookies: {
+//         getAll() {
+//           return request.cookies.getAll()
+//         },
+//         setAll(cookiesToSet) {
+//           cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value))
+//           supabaseResponse = NextResponse.next({
+//             request,
+//           })
+//           cookiesToSet.forEach(({ name, value, options }) =>
+//             supabaseResponse.cookies.set(name, value, options)
+//           )
+//         },
+//       },
+//     }
+//   )
+
+//   const {
+//     data: { user },
+//   } = await supabase.auth.getUser()
+
+  return supabaseResponse
+}
