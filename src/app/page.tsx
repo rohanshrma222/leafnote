c21399729ffb54ec71a5536e7d35d1a31f6948df
unknown <hackerviruse04@gmail.com>
2025-04-23 14:57:25 +0530

    making the note section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,36 @@
-import React from "react";
+import { getUser } from "@/auth/server";
+import AskAIButton from "@/components/AskAIButton";
+import NewNoteButton from "@/components/NewNoteButton";
+import NoteTextInput from "@/components/NoteTextInput";
+import HomeToast from "@/components/HomeToast";
+import { prisma } from "../../prisma/prisma";
 
-function HomePage() {
+type Props = {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+async function HomePage({ searchParams }: Props) {
+  const noteIdParam = (await searchParams).noteId;
+  const user = await getUser();
+
+  const noteId = Array.isArray(noteIdParam)
+    ? noteIdParam![0]
+    : noteIdParam || "";
+
+  const note = await prisma.note.findUnique({
+    where: { id: noteId, authorId: user?.id },
+  });
+
   return (
-    <div className="flex flex-col items-center justify-center p-4 text-center">
-      HomePage
+    <div className="flex h-full flex-col items-center gap-4">
+      <div className="flex w-full max-w-4xl justify-end gap-2">
+        <AskAIButton user={user} />
+        <NewNoteButton user={user} />
+      </div>
+
+      <NoteTextInput noteId={noteId} startingNoteText={note?.text || ""} />
+
+      <HomeToast />
     </div>
   );
 }
